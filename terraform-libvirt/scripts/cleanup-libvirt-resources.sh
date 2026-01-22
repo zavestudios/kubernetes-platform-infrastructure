ba9285c9b2bfcc4f8d66196b4fd51f9b80eb125e
Xavier Lopez <ecks910@yahoo.com>
2026-01-22 11:46:42 -0800

    wip

--- terraform-libvirt/scripts/cleanup-libvirt-resources.sh
+++ terraform-libvirt/scripts/cleanup-libvirt-resources.sh
@@ -16,13 +16,24 @@ for vm in k3s-cp-01 k3s-worker-01 k3s-worker-02; do
     fi
 done
 
-# Delete volumes
-echo "Deleting volumes..."
+# Delete volumes from libvirt pool
+echo "Deleting volumes from libvirt pool..."
 for vol in k3s-cp-01.qcow2 k3s-worker-01.qcow2 k3s-worker-02.qcow2 \
            k3s-cp-01-cloudinit.iso k3s-worker-01-cloudinit.iso k3s-worker-02-cloudinit.iso; do
     if sudo virsh vol-list --pool xlopez | grep -q "$vol"; then
-        echo "  Deleting $vol..."
+        echo "  Deleting $vol from pool..."
         sudo virsh vol-delete --pool xlopez "$vol" 2>/dev/null || true
+    fi
+done
+
+# Delete files directly from filesystem (catches anything libvirt doesn't know about)
+echo "Cleaning up filesystem..."
+cd /home/xlopez/libvirt_images
+for file in k3s-cp-01.qcow2 k3s-worker-01.qcow2 k3s-worker-02.qcow2 \
+            k3s-cp-01-cloudinit.iso k3s-worker-01-cloudinit.iso k3s-worker-02-cloudinit.iso; do
+    if [ -f "$file" ]; then
+        echo "  Removing $file..."
+        sudo rm -f "$file"
     fi
 done
 
